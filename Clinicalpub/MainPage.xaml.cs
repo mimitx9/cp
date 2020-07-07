e1bf10c24428c4e341fdceca2412d0f816f276ee
Lê Tuấn Anh <c.robin9x@gmail.com>
2020-07-07 07:30:22 +0700

    Ta Ta

---
+++ Clinicalpub/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Clinicalpub
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            webView.Source = "https://videdental.com";
+        }
+
+        private async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        }
+
+        private void OnForwardButtonClicked(object sender, EventArgs e)
+        {
+            webView.Source = "https://videdental.com";
+        }
+
+    }
+}
